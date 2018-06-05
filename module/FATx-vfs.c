71aac22f0267600ca6ec069eb25310c40fc35622
bjoern <bjoern.daase@student.hpi.de>
2018-06-05 10:34:50 +0200

    Enhance kernel module, for some reason we don't see the degug messages

--- module/FATx-vfs.c
+++ module/FATx-vfs.c
@@ -14,15 +14,45 @@ MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");
 
 static int fatx_fill_super(struct super_block *sb, void *data, int silent)
 {
-  //Here we have to make the whole super block definition. Here will the un begin...
-  return 0;
+    /*struct inode *root = NULL;
+
+    sb->s_magic = AUFS_MAGIC_NUMBER;
+    sb->s_op = &aufs_super_ops;
+
+    root = new_inode(sb);
+    if (!root)
+    {
+         pr_err("inode allocation failed\n");
+         return -ENOMEM;
+    }
+
+    root->i_ino = 0;
+    root->i_sb = sb;
+    root->i_atime = root->i_mtime = root->i_ctime = CURRENT_TIME;
+    inode_init_owner(root, NULL, S_IFDIR);
+
+    sb->s_root = d_make_root(root);
+    if (!sb->s_root)
+    {
+        pr_err("root creation failed\n");
+        return -ENOMEM;
+    }*/
+
+   return 0;
 }
 
 static struct dentry *fatx_mount(struct file_system_type *fs_type, int flags, const char *dev_name, void *data)
 {
 
   printk(KERN_INFO "fatx: devname = %s\n", dev_name);
-  return mount_bdev(fs_type, flags, dev_name, data, fatx_fill_super);
+  struct dentry *entry = mount_bdev(fs_type, flags, dev_name, data, fatx_fill_super);
+
+  if (IS_ERR(entry))
+          pr_err("fat mounting failed\n");
+      else
+          pr_debug("fatx mounted\n");
+
+  return entry;
 
 }
 
@@ -37,16 +67,16 @@ static struct file_system_type fatx_fs_type = {
 static int __init FATxvfs__init(void)
 {
 
-   printk(KERN_INFO "Hello from the FATxvfs LKM!\n");
-   return register_filesystem(&fatx_fs_type);
+  printk(KERN_INFO "Hello from the FATxvfs LKM!\n");
+  return register_filesystem(&fatx_fs_type);
 
 }
 
 static void __exit FATxvfs__exit(void)
 {
 
-   printk(KERN_INFO "Goodbye from the FATxvfs LKM!\n");
-   unregister_filesystem(&fatx_fs_type);
+  printk(KERN_INFO "Goodbye from the FATxvfs LKM!\n");
+  unregister_filesystem(&fatx_fs_type);
 
 }
 
