fb0d8a09107b2f0179b2cb1902fcd1c9c369a495
bjoern <bjoern.daase@student.hpi.de>
2018-06-16 12:09:26 +0200

    Add id information via statfs

--- module/super.c
+++ module/super.c
@@ -43,9 +43,13 @@ static int
 fatx_statfs(struct dentry *dentry, struct kstatfs *buf)
 {
   struct super_block *sb = dentry->d_sb;
+  struct fatx_super_info *sbi = FATX_SB(sb);
+  u64 id = huge_encode_dev(sb->s_bdev->bd_dev);
 
-  buf->f_type = FATX_MAGIC_NUMBER;
+  buf->f_type = dentry->d_sb->s_magic;
   buf->f_bsize = sb->s_blocksize;
+  buf->f_fsid.val[0] = (u32)id;
+  buf->f_fsid.val[1] = (u32)(id >> 32);
 
   return 0;
 }
